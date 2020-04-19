8617412d5dc6c7b363e28a274bba27323a459af1
caec08 <novacentauri42@gmail.com>
2020-04-19 16:24:11 -0700

    Fix mentions

--- NoU.py
+++ NoU.py
@@ -13,9 +13,9 @@ from dotenv import load_dotenv
 from datetime import datetime
 
 if __name__ == "__main__":
-    
+
     print("Starting...")
-    
+
     # Get environment
     load_dotenv()
     token = os.getenv("DISCORD_TOKEN")
@@ -26,21 +26,21 @@ if __name__ == "__main__":
     @client.event
     async def on_ready():
         print(f'{client.user} has connected to Discord!')
-    
+
     @client.event
     async def on_message(message):
         if message.author == client.user:
             return
-        
+
         content = message.content
         content = content.lower().strip()
         content = ' '.join(content.split())
         content = content.translate(str.maketrans("", "", string.punctuation))
-        
+
         if content == "no u":
             try:
-                await message.channel.send(f'@{message.author} No U')
+                await message.channel.send(f'{message.author.mention} No U')
             except:
                 print("Could not reply")
-    
+
     client.run(token)
